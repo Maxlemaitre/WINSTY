5b186b04d88c6d82146160415ca6083b61eaa872
maxime le maitre <maximelm25@gmail.com>
2017-04-04 21:49:55 +0200

    solve few problems on speech creation process

--- app/assets/javascripts/newspeech.js
+++ app/assets/javascripts/newspeech.js
@@ -3,10 +3,24 @@ $(document).ready(function() {
     $(".introduction-form").slideUp(700);
     $(".first-form").removeClass("hide");
   });
-  $(".first-form").delay(4000).slideUp(700);
 
-  setTimeout(function() {
-       $(".second-form").removeClass("hide");
-   }, 4000);
+    $(".first-form").delay(4000).slideUp(700);
+    setTimeout(function(){
+      $(".second-form").removeClass("hide")},4000);
 });
 
+
+
+// La vrai fonction à utiliser
+
+// $(document).ready(function() {
+//   $(".start-speech").click(function(event) {
+//     $(".introduction-form").slideUp(700);
+//     $(".first-form").removeClass("hide");
+//   });
+
+//   ZiggeoApi.Events.on("submitted", function (data) {
+//     $(".first-form").delay(4000).slideUp(700);
+//     $(".second-form").removeClass("hide")
+//   })
+// });
