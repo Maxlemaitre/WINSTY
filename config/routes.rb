1a8f13d1b610e1908f32f49e421a359d997badcc
maxime le maitre <maximelm25@gmail.com>
2017-04-10 20:06:52 +0200

    generate the reviews controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'speeches#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :speeches
+  resources :speeches do
+    resources :reviews, only: [:new, :create]
+  end
 end
