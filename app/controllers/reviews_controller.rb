fcf01c3d230d5d1f74c94519b031739e1940a25c
maxime le maitre <maximelm25@gmail.com>
2017-04-11 12:45:50 +0200

    added the review + integrated filtering method on the speech index

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,18 @@
 class ReviewsController < ApplicationController
+  before_action :set_speech, only: :create
+
   def new
+    @review = Review.new
   end
 
   def create
+    @review = Review.create(user: current_user, speech: @speech, content: params[:review][:content])
+    redirect_to speech_path(@speech)
+  end
+
+  private
+
+  def set_speech
+    @speech = Speech.find(params[:speech_id])
   end
 end
