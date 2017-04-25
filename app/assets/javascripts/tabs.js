809712f61a06d0cc0cc51549cbcaa0d24acf5e47
maxime le maitre <maximelm25@gmail.com>
2017-04-25 11:40:45 +0200

    problem of active tabs when you refresh the page fixed

--- app/assets/javascripts/tabs.js
+++ app/assets/javascripts/tabs.js
@@ -1,13 +1,22 @@
 $(document).ready(function() {
-  var div_active = "div#" + $(".active").attr('id');
-  $("div.tab-content").hide();
-  $(div_active).show();
-  $(".tab").click(function(event) {
+  if (window.location.hash != "") {
+    var anchor = window.location.hash;
+    var div_active = "div" + anchor
+    $("div.tab-content").hide();
+    $(div_active).show();
     $(".tab").removeClass("active");
-    $(this).addClass("active");
+    $(anchor).addClass("active");
+  } else {
     var div_active = "div#" + $(".active").attr('id');
     $("div.tab-content").hide();
     $(div_active).show();
+  };
+  $(".tab").click(function(event) {
+  $(".tab").removeClass("active");
+  $(this).addClass("active");
+  var div_active = "div#" + $(".active").attr('id');
+  $("div.tab-content").hide();
+  $(div_active).show();
   });
 });
 
